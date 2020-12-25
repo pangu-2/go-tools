e3de66d6ac3035067e1fcfc3a1ba656da977345c
fox <foxwho@aliyun.com>
2020-12-25 13:30:26 +0800

    add StrToInt64

--- tools/convUtil/StrToInt.go
+++ tools/convUtil/StrToInt.go
@@ -6,3 +6,11 @@ func StrToInt(str string) int {
 	i, _ := strconv.Atoi(str)
 	return i
 }
+
+func StrToInt64(str string) int64 {
+	i, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		return 0
+	}
+	return i
+}
