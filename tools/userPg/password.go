cf11eaa7b8f4e5b5987ce5e17112c55cebdc297c
fox <foxwho@aliyun.com>
2022-11-12 21:59:39 +0800

    密码加密

--- tools/userPg/password.go
+++ tools/userPg/password.go
@@ -9,15 +9,16 @@ func PasswordSalt(pass, salt string) string {
 	salt1 := "4%$@w"
 	str := salt1 + pass + salt
 	//return cryptUtil.Md5(cryptUtil.Sha256(str))
-	return cryptPg.Md5(str)
+	//return cryptPg.Md5(str)
+	return SaltMake(str, salt)
 }
 
-//验证
+// 验证
 func PasswordVerify(password, pass, salt string) bool {
 	return password == PasswordSalt(pass, salt)
 }
 
-//密码加密
+// 密码加密
 func SaltMake(str, salt string) string {
 	return cryptPg.Md5(cryptPg.Sha256(str+salt) + salt)
 }
