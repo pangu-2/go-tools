56be86a6605bbc85ff3eb528067b318d009bb7e4
fox <foxwho@aliyun.com>
2025-06-04 20:22:35 +0800

    ToUint64

--- tools/strPg/number.go
+++ tools/strPg/number.go
@@ -2,27 +2,32 @@ package strPg
 
 import "github.com/pangu-2/go-tools/tools/numberPg"
 
-//字符串转换成  int 没有错误返回
+// 字符串转换成  int 没有错误返回
 func ToInt(str string) int {
 	return numberPg.StrToInt(str)
 }
 
-//字符串转换成  int64 没有错误返回
+// 字符串转换成  int64 没有错误返回
 func ToInt64(str string) int64 {
 	return numberPg.StrToInt64(str)
 }
 
-//字符串转换成  float64 没有错误返回
+// ToUint64  字符串转换成  uint64 没有错误返回
+func ToUint64(str string) uint64 {
+	return numberPg.StrToUInt64(str)
+}
+
+// 字符串转换成  float64 没有错误返回
 func ToFloat64(str string) float64 {
 	return numberPg.StrToFloat64(str)
 }
 
-//字符串转换成  float64 没有错误返回
+// 字符串转换成  float64 没有错误返回
 func ToFloat64FormInterface(str interface{}) float64 {
 	return numberPg.StrToFloat64FormInterface(str)
 }
 
-//字符串转换成  int 没有错误返回
+// 字符串转换成  int 没有错误返回
 func ToIntFormInterface(str interface{}) int {
 	return numberPg.StrToIntFormInterface(str)
 }
